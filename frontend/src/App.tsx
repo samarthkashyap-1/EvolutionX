c300ae04db1ba6ae29349fb53c893c67f75386b1
samarth <samarthkashyap318@gmail.com>
2024-07-17 17:00:55 +0530

    chore: Update lazy loading for components in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,20 @@
-import { useState, useEffect } from "react";
-import LandingPage from "./pages/LandingPage";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Register from "./components/Register";
+import { useState, useEffect, lazy } from "react";
 import Lottie from "lottie-react";
 import pokiball from "./assets/pokeball-loader.json";
 import { Routes, Route } from "react-router-dom";
-import PokemonDisplay from "./components/AdoptionCenter";
-import MyPokemons from "./components/MyPokemons";
-import TopPlayer from "./components/TopPlayer";
 import { getAllUsers } from "./services/api";
 import { useRecoilState } from "recoil";
 import { allUser, User, UserData } from "./recoil/atom";
 import { Toaster } from "react-hot-toast";
-import Notfound from "./components/Notfound";
+const Notfound = lazy(() => import("./components/Notfound"));
+const PokemonDisplay = lazy(() => import("./components/AdoptionCenter"));
+const MyPokemons = lazy(() => import("./components/MyPokemons"));
+const TopPlayer = lazy(() => import("./components/TopPlayer"));
+const LandingPage = lazy(() => import("./pages/LandingPage"));
+const Navbar = lazy(() => import("./components/Navbar"));
+const Home = lazy(() => import("./components/Home"));
+const Login = lazy(() => import("./components/Login"));
+const Register = lazy(() => import("./components/Register"));
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -66,7 +66,7 @@ function App() {
           <Route path="/adoptioncenter" element={<PokemonDisplay />} />
           <Route path="/profile/:id" element={<MyPokemons />} />
           <Route path="/topplayer" element={<TopPlayer />} />
-          <Route path="*" element={<Notfound/>} />
+          <Route path="*" element={<Notfound />} />
         </Route>
       </Routes>
     </div>
